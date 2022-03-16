145856df511e57529fbe2ba736df8c83bb049adc
Vanessa <hello@vfernandes.dev>
2022-03-16 16:19:52 +0100

    Iteration 1 done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
+const bcrypt = require("bcrypt");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+router.post("/signup", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const salt = await bcrypt.genSalt(12);
+  const hash = await bcrypt.hash(password, salt);
+
+  await User.create({username, password: hash});
+
+  res.render("userProfile")
+})
+
 
 module.exports = router;
